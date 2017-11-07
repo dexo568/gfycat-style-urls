c73b9a48c537aefdf2a3d0be7b79af89982b2ace
Chris Ozols <chris.v.ozols@gmail.com>
2017-11-06 20:47:08 -0600

    removed console log

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const adjectives = require('./adjectives'),
 module.exports = {
 	generateCombination(numAdjectives, delimiter) {
 		let combination = '';
-		console.log('Hello, World!');
+
 		for (let i = 0; i < numAdjectives; i++) {
 			combination += adjectives[Math.floor(Math.random() * adjectives.length)] + delimiter;
 		}
+
 		combination += animals[Math.floor(Math.random() * animals.length)];
 		return combination;
 	}
