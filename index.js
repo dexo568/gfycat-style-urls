032a23c30dd8f142957aa41b124a284f08af9164
Chris Ozols <chris.v.ozols@gmail.com>
2017-11-26 14:27:16 -0600

    added a 'capitalize first letter' argument, removed a few animals that had spaces in their names

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ const adjectives = require('./adjectives'),
 module.exports = {
 	generateCombination(numAdjectives, delimiter, capitalizeFirstLetter) {
 		let combination = '';
+		const animal = animals[Math.floor(Math.random() * animals.length)];
 
 		for (let i = 0; i < numAdjectives; i++) {
-			combination += capitalizeFirstLetter
-				? adjectives[Math.floor(Math.random() * adjectives.length)].charAt(0).toUpperCase() + delimiter
-				: adjectives[Math.floor(Math.random() * adjectives.length)] + delimiter;
+			const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+
+			combination += capitalizeFirstLetter ? adjective.charAt(0).toUpperCase() + adjective.slice(1) + delimiter : adjective + delimiter;
 		}
 
-		combination += capitalizeFirstLetter
-			? animals[Math.floor(Math.random() * animals.length)].charAt(0).toUpperCase()
-			: animals[Math.floor(Math.random() * animals.length)];
+		combination += capitalizeFirstLetter ? animal.charAt(0).toUpperCase() + animal.slice(1) : animal;
 		return combination;
 	}
 };
